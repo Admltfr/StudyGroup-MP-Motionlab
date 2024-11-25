d2d6314eed79941714ccd2de14ec2b29e42d9380
admltfr <adamlutfiramadhan@gmail.com>
2024-11-25 10:37:59 +0700

    perbaikan singleuserbank

--- singleuserbank.dart
+++ singleuserbank.dart
@@ -2,12 +2,7 @@ import 'dart:io';
 
 class total {
   int amount;
-  total(this.amount);
-}
-void main(){
-  total balance = total(0);
-  void checkBalance(total balance) => print("Currently your balance is ${balance.amount} \$");
-  void depositBalance(total balance, int deposit) => balance.amount += deposit;
+
   void withdrawBalance(total balance, int withdraw) {
     if (balance.amount - withdraw >= 0) {
       balance.amount -= withdraw;
@@ -15,6 +10,15 @@ void main(){
       print("Insufficient balance");
     }
   }
+  void checkBalance(total balance) => print("Currently your balance is ${balance.amount} \$");
+  void depositBalance(total balance, int deposit) => balance.amount += deposit;
+
+  total(this.amount);
+}
+void main(){
+
+  total balance = new total(0);
+  
   final Function() divider = () => print("----------------------------------------");
   final Function() Menu = () {
     divider();
@@ -23,12 +27,26 @@ void main(){
     stdout.write("Choose menu: ");
   };
   final Function() endProgram = () => print("Thanks for using this app");
+  final Function() switchMenuErr = () {
+    print("Please select a valid menu");
+    divider();};
+
+
+  String optionValidator(List<String> optionList, String? options) {
+    while (!optionList.contains(options)) {
+      stdout.write("Please enter a valid option $optionList : ");
+      options = stdin.readLineSync();
+    }
+    divider();
+    return options!;
+  }
 
   Menu();
   String? switchMenu = stdin.readLineSync(); 
+  switchMenu = optionValidator(["0","1","2","3"], switchMenu);
   while (switchMenu != "0") {
     if (switchMenu == "1") {
-      checkBalance(balance);
+      balance.checkBalance(balance);
       if (balance.amount == 0) {
         print("Try to deposit some balance to this account");
       }
@@ -36,22 +54,19 @@ void main(){
     } else if (switchMenu == "2") {
       stdout.write("Enter the amount you want to deposit: ");
       int deposit = int.parse(stdin.readLineSync()!);
-      depositBalance(balance, deposit);
+      balance.depositBalance(balance, deposit);
       divider();
     } else if (switchMenu == "3") {
       stdout.write("Enter the amount you want to withdraw: ");
       int withdraw = int.parse(stdin.readLineSync()!);
-      withdrawBalance(balance, withdraw);
+      balance.withdrawBalance(balance, withdraw);
       divider();
     } else {
-      final Function() switchMenuErr = () =>
-        print("Please select a valid menu");
-      ;
       switchMenuErr();
-      divider();
     }
     Menu();
-    switchMenu = stdin.readLineSync();
+    switchMenu = stdin.readLineSync(); 
+    switchMenu = optionValidator(["0","1","2","3"], switchMenu);
   }
   endProgram();
 }
